fdc05752dac6de26f700d8e1e9502d24bc796398
M1K1B <miki.balabanovic111@gmail.com>
2023-01-19 11:17:51 +0100

    Tested SEO, added SEO data and added mailto: in contact

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,34 +1,34 @@
 module.exports = {
-	publicRuntimeConfig: {
-		// Will be available on both server and client
-		// Pass through env variables
-		baseUrl: process.env.BASE_URL,
-		baseApiUrl: process.env.API_URL,
-		staticFolder: process.env.STATIC_DIR,
-		// Will be used for per SEO page default
-		baseSeo: {
-			robotsProps: {
-				maxSnippet: -1,
-				maxImagePreview: "none",
-				maxVideoPreview: -1,
-			},
-		},
-		name: "Negovatelji", // TODO
-		title: "Negovatelji u Srbiji",
-		slogan: "Negujmo one koji su i nas nekada negovali", // TODO (is it even used)
-		description: "Negovatelji i osobe koje traže negu u Srbiji", // TODO
-		author: process.env.AUTHOR, // TODO?
-		logo: `images/logo.svg`, // TODO
-		imageShare: `images/static.2.jpg`, // TODO
-		facebookUrl: "https://negovatelji.rs", // TODO
-		twitterUrl: "https://negovatelji.rs", // TODO
-		instagramUrl: "https://negovatelji.rs", // TODO
-		linkedInUrl: "https://negovatelji.rs", // TODO
-		phone: "+381000000000", // TODO
-		address: "Novi Sad", // TODO
-		region: "Vojvodina",
-		country: "RS",
-		postalCode: "21000",
-		locale: "sr",
-	},
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    // Pass through env variables
+    baseUrl: process.env.BASE_URL,
+    baseApiUrl: process.env.API_URL,
+    staticFolder: process.env.STATIC_DIR,
+    // Will be used for per SEO page default
+    baseSeo: {
+      robotsProps: {
+        maxSnippet: -1,
+        maxImagePreview: "none",
+        maxVideoPreview: -1,
+      },
+    },
+    name: "Negovatelji - Zajednica brižnih",
+    title: "Negovatelji u Srbiji",
+    slogan: "Negujmo one koji su i nas nekada negovali",
+    description:
+      "Negovatelji.rs je platforma za povezivanje, osnaživanje i podršku svima koji neguju bližnje, nemoćne, zavisne osobe… za sve kojima je potrebna pomoć za kućnu negu bolesnika, dece, supružnika, roditelja, prijatelja…",
+    author: process.env.AUTHOR,
+    logo: `images/logo.svg`,
+    imageShare: `images/static.2.jpg`,
+    facebookUrl: "https://www.facebook.com/groups/270285275225942/",
+    instagramUrl: "https://instagram.com/negovatelji?igshid=MWI4MTIyMDE=",
+    youtubeUrl: "https://www.youtube.com/channel/UCS5gh6FEudKxWeUPIzL_Kog",
+    phone: "+381000000000", // TODO
+    address: "Novi Sad",
+    region: "Vojvodina",
+    country: "RS",
+    postalCode: "21000",
+    locale: "sr",
+  },
 };
