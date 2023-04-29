ba6945e206f12ef3e270afa7c45906bfc364a5c5
Milos Roknic <roknic.milos.994@gmail.com>
2023-04-29 17:05:45 +0200

    API | Add is_financial filter to Donations endpoint and generate missing migrations

--- backend/src/apps/donations/filters.py
+++ backend/src/apps/donations/filters.py
@@ -6,10 +6,12 @@ from apps.donations.models import Donation
 class DonationFilterSet(FilterSet):
     contains = CharFilter(field_name='title', lookup_expr='icontains')
     is_active = BooleanFilter(field_name='is_active')
+    is_financial = BooleanFilter(field_name='financial_info', lookup_expr='isnull', exclude=True)
 
     class Meta:
         model = Donation
         fields = [
             'title',
             'is_active',
+            'is_financial',
         ]
