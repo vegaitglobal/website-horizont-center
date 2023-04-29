6a299e5be57c0ec0ff7f7daf2487db9d397e75b8
Milos Roknic <roknic.milos.994@gmail.com>
2023-04-29 16:04:56 +0200

    APP | Increase font-weight for Contact page text

--- frontend/src/components/contact-page-content/contact.page.content.jsx
+++ frontend/src/components/contact-page-content/contact.page.content.jsx
@@ -3,19 +3,19 @@ import { PageHeader, SectionWithImage } from "shared-components";
 import styles from "./contact.page.content.module.scss";
 
 export const ContactPageContent = () => {
-  return (
-    <div className={styles.contactPageContent}>
-      <PageHeader withBackground title={"Povežimo se"} />
-      <SectionWithImage
-        imageSrc="/images/static.4.jpg"
-        paragraphs={
-          <>
-            <p>Centar HORIZONT 21</p>
-            <p>Novi Sad</p>
-            <a href="mailto:zajednica@negovatelji.rs">zajednica@negovatelji.rs</a>
-          </>
-        }
-      />
-    </div>
-  );
+	return (
+		<div className={styles.contactPageContent}>
+			<PageHeader withBackground title={"Povežimo se"}/>
+			<SectionWithImage
+				imageSrc="/images/static.4.jpg"
+				paragraphs={
+					<>
+						<p className={styles.bold}>Centar HORIZONT 21</p>
+						<p className={styles.bold}>Novi Sad</p>
+						<a href="mailto:zajednica@negovatelji.rs">zajednica@negovatelji.rs</a>
+					</>
+				}
+			/>
+		</div>
+	);
 };
