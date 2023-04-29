b8160e9c959edcbfa2f05c414968f40a21a9d483
Milos Roknic <roknic.milos.994@gmail.com>
2023-04-29 18:03:45 +0200

    API | Remove is_published filter and automatically filter only published Blogs

--- backend/src/apps/blogs/filters.py
+++ backend/src/apps/blogs/filters.py
@@ -6,12 +6,10 @@ from apps.blogs.models import Blog
 class BlogFilterSet(FilterSet):
     contains = CharFilter(field_name='title', lookup_expr='icontains')
     category_name = CharFilter(field_name='categories__name')
-    is_published = BooleanFilter(field_name='is_published')
 
     class Meta:
         model = Blog
         fields = [
             'title',
             'categories',
-            'is_published',
         ]
--- backend/src/apps/blogs/viewsets.py
+++ backend/src/apps/blogs/viewsets.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from django_filters import rest_framework as filters
 from apps.blogs.filters import BlogFilterSet
 from apps.blogs.models import Blog
@@ -10,3 +11,6 @@ class BlogViewSet(ViewSet):
     serializer_class = BlogSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = BlogFilterSet
+
+    def filter_queryset(self, queryset) -> QuerySet:
+        return super().filter_queryset(queryset=queryset.filter(is_published=True))
