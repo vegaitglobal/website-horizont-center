5fbd3cd9620128fcaa914c329cebf87bc56b5932
Milos Roknic <roknic.milos.994@gmail.com>
2023-04-29 17:12:41 +0200

    APP | Replace 'contains' filter wiht 'isFinancial' on Donations page

--- frontend/src/components/resource-filters/donation.filters.jsx
+++ frontend/src/components/resource-filters/donation.filters.jsx
@@ -1,4 +1,4 @@
-import { Input, Select } from "../../shared-components";
+import { Select } from "../../shared-components";
 import styles from "./filters.module.scss";
 
 export function DonationFilters ({ onChange }) {
@@ -6,16 +6,16 @@ export function DonationFilters ({ onChange }) {
 		<div className={styles.filtersWrapper}>
 			<p className={styles.explanatoryText}>Pretražite donacije na osnovu sledećih parametara:</p>
 			<div className={styles.filters}>
-				<Input
-					withSearchIcon
-					className={styles.leftField}
-					id="donationContains"
-					name="donationContains"
-					placeholder="Naziv donacije..."
-					onChange={(value) => onChange({ contains: value })}
+				<Select
+					className={styles.field}
+					id="donationType"
+					name="donationType"
+					options={{ true: "finansijska", false: "robna" }}
+					placeholder="Vrsta donacije..."
+					onChange={(value) => onChange({ isFinancial: value })}
 				/>
 				<Select
-					className={styles.rightField}
+					className={styles.field}
 					id="donationStatus"
 					name="donationStatus"
 					options={{ true: "u toku", false: "završeno" }}
